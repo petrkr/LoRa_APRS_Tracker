4c8765b8569d6542c9eff91e509e4132230ca97a
Petr Kracik <petrkr@petrkr.net>
2024-07-03 18:20:11 +0200

    Support OctopusLab ESP32C3 Board with SX1268 Lora radio
    Signed-off-by: Petr Kracik <petrkr@petrkr.net>

--- src/boards_pinout.h
+++ src/boards_pinout.h
@@ -238,4 +238,19 @@
     #define RADIO_BUSY_PIN      2
 #endif
 
+#if defined(ESP32_C3_OctopusLab_LoRa)
+    #define OLED_SDA            0
+    #define OLED_SCL            1
+    #define OLED_RST            -1
+    #define GPS_RX              -1
+    #define GPS_TX              -1
+    #define RADIO_SCLK_PIN      6
+    #define RADIO_MISO_PIN      4
+    #define RADIO_MOSI_PIN      7
+    #define RADIO_CS_PIN        5
+    #define RADIO_DIO1_PIN      3
+    #define RADIO_RST_PIN       -1
+    #define RADIO_BUSY_PIN      8
+#endif
+
 #endif
